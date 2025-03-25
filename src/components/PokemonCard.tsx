763233df0afeab1bfcffb8564abe60fe1658daa1
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-03-25 14:56:24 +0100

    pokedex step3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,19 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} />}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
 }
 
