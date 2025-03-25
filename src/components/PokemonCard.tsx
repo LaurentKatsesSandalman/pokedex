385de04f6df4063c42705eb24564ffde8ad85c7e
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-03-25 15:39:12 +0100

    pokemon is now a prop

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,18 +1,8 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
-function PokemonCard() {
-    const pokemon = pokemonList[1];
+
+function PokemonCard({ pokemon }) {
     return <figure>
-        {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} />}
+        {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name} />}
         <figcaption>{pokemon.name}</figcaption>
     </figure>
 }
